0a6fb8463b7a863e64c557015fe7207f9d9ba6b4
Ignacio Villarreal <ignaciovillarreal20031231@gmail.com>
2025-09-30 15:16:26 -0300

    feat: Initial commit of Spring Boot project

---
+++ src/main/java/iv/synapseuser/SynapseUserApplication.java
@@ -0,0 +1,13 @@
+package iv.synapseuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapseUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iv/synapseuser/SynapseUserApplicationTests.java
@@ -0,0 +1,13 @@
+package iv.synapseuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
